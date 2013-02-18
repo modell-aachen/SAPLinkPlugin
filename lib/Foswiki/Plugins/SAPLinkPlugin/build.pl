171086313eabb5af42b8a408d2d412dac61b221b
Stephan Osthold <osthold@modell-aachen.de>
2013-02-18 10:59:31 +0100

    build-Script durch Vererbung realisiert.
    Ticket: none

--- lib/Foswiki/Plugins/SAPLinkPlugin/build.pl
+++ lib/Foswiki/Plugins/SAPLinkPlugin/build.pl
@@ -2,23 +2,35 @@
 BEGIN { unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} ); }
 use Foswiki::Contrib::Build;
 
-# Create the build object
-$build = new Foswiki::Contrib::Build('SAPLinkPlugin');
+package SAPLinkPluginBuild;
+our @ISA = qw( Foswiki::Contrib::Build );
 
-# (Optional) Set the details of the repository for uploads.
-# This can be any web on any accessible Foswiki installation.
-# These defaults will be used when expanding tokens in .txt
-# files, but be warned, they can be overridden at upload time!
 
-# name of web to upload to
-$build->{UPLOADTARGETWEB} = 'Extensions';
-# Full URL of pub directory
-$build->{UPLOADTARGETPUB} = 'http://modell-aachen.de';
-# Full URL of bin directory
-$build->{UPLOADTARGETSCRIPT} = 'http://modell-aachen.de';
-# Script extension
-$build->{UPLOADTARGETSUFFIX} = '';
+sub new {
+    my $class = shift;
+    return bless( $class->SUPER::new( 'SAPLinkPlugin' ));
+}
 
+# Will first symlink _source files to plugins directory and do a standard build
+sub target_build {
+    my $this = shift;
+
+    # Symlink the source to ..._src.js files and return to current dir
+    my $dir = "$this->{basedir}/pub/System/CKEditorPlugin/ckeditor/_source";
+    my $saveddir = `pwd`;
+    $saveddir =~ m#(.*)#; # filter out newline
+    $saveddir = $1;
+    print "Symlinking _source...\n";
+    _linkFiles($dir);
+    chdir $saveddir;
+
+    # do a standard build
+    $this->SUPER::target_build(@ARGV);
+}
+
+# This function will recursively call itself and link files from _source to
+# the plugins-dir with _src suffix where appropriate.
+# Initial call should point $dir to _source directory.
 sub _linkFiles {
     my ( $dir ) = @_;
     print "processing dir $dir\n";
@@ -45,19 +57,25 @@ sub _linkFiles {
     }
 }
 
-my $dir = `pwd`;
-my $saveddir = $dir;
-$dir =~ m#(.*)#; # filter out newline
-$dir = $1;
-unless ( -e "$dir/build.pl" && $dir =~ m#/lib/Foswiki/Plugins/SAPLinkPlugin$# ) {
-    print "\nRunning from '$dir'\nPlease call from SAPLinkPlugin/lib/Foswiki/Plugins/SAPLinkPlugin/\n\n";
-    exit 1;
-}
-$dir =~ s#/lib/Foswiki/Plugins/SAPLinkPlugin$#/pub/System/CKEditorPlugin/ckeditor/_source#;
-print "Symlinking _source...\n";
-_linkFiles($dir);
 
-chdir $saveddir;
+# Create the build object
+$build = new SAPLinkPluginBuild();
+
+# Standard build script from here on...
+
+# (Optional) Set the details of the repository for uploads.
+# This can be any web on any accessible Foswiki installation.
+# These defaults will be used when expanding tokens in .txt
+# files, but be warned, they can be overridden at upload time!
+
+# name of web to upload to
+$build->{UPLOADTARGETWEB} = 'Extensions';
+# Full URL of pub directory
+$build->{UPLOADTARGETPUB} = 'http://modell-aachen.de';
+# Full URL of bin directory
+$build->{UPLOADTARGETSCRIPT} = 'http://modell-aachen.de';
+# Script extension
+$build->{UPLOADTARGETSUFFIX} = '';
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
